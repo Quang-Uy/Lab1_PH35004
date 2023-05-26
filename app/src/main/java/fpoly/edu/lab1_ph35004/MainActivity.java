07fd82b732e237ce59c4df770dd5bc55f06de6e7
Quang Uy <uybqph35004@fpt.edu.vn>
2023-05-26 15:56:10 +0700

    Lab1

---
+++ app/src/main/java/fpoly/edu/lab1_ph35004/MainActivity.java
@@ -0,0 +1,14 @@
+package fpoly.edu.lab1_ph35004;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
